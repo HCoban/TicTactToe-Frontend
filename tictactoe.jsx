e4f42c07990060d380b8cc5f67c07b7a7d70f48d
Halil Coban <halil.coban@gmail.com>
2018-01-26 18:30:54 -0800

    initialized app

---
+++ tictactoe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <h1>Welcome to Tic Tac Toe Game!</h1>,
+    document.getElementById('root')
+  );
+});
